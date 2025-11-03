4438f5b78ec1b61f7b0933876b139803178ff26a
김태원 <kronak2355@gmail.com>
2025-11-03 10:28:37 +0900

    doit write

--- webproxy-lab/tiny/tiny.c
+++ webproxy-lab/tiny/tiny.c
@@ -14,8 +14,7 @@ int parse_uri(char *uri, char *filename, char *cgiargs);
 void serve_static(int fd, char *filename, int filesize);
 void get_filetype(char *filename, char *filetype);
 void serve_dynamic(int fd, char *filename, char *cgiargs);
-void clienterror(int fd, char *cause, char *errnum, char *shortmsg,
-                 char *longmsg);
+void clienterror(int fd, char *cause, char *errnum, char *shortmsg, char *longmsg);
 
 int main(int argc, char **argv)
 {
@@ -42,5 +41,58 @@ int main(int argc, char **argv)
     printf("Accepted connection from (%s, %s)\n", hostname, port);
     doit(connfd);  // line:netp:tiny:doit
     Close(connfd); // line:netp:tiny:close
+  }
+}
+
+void doit(int fd)
+{
+  int is_static;
+  struct statsbuf;
+  char buf[MAXLINE], method[MAXLINE], uri[MAXLINE], version[MAXLINE];
+  char filename[MAXLINE], cgiargs[MAXLINE];
+  rio_t rio;
+
+  /*Readrequestlineandheaders*/
+  Rio_readinitb(&rio, fd);
+  Rio_readlineb(&rio, buf, MAXLINE);
+  printf("Requestheaders:\n");
+  printf("%s", buf);
+  sscanf(buf, "%s%s%s", method, uri, version);
+  if (strcasecmp(method, "GET"))
+  {
+    clienterror(fd, method, "501", "Notimplemented",
+                "Tinydoesnotimplementthismethod");
+    return;
+  }
+  read_requesthdrs(&rio);
+
+  /*ParseURIfromGETrequest */
+  is_static = parse_uri(uri, filename, cgiargs);
+  if (stat(filename, &sbuf) < 0)
+  {
+    clienterror(fd, filename, "404", "Notfound",
+                "Tinycouldn’tfindthisfile");
+    return;
+  }
+
+  if (is_static)
+  { /*Servestaticcontent*/
+    if (!(S_ISREG(sbuf.st_mode)) || !(S_IRUSR & sbuf.st_mode))
+    {
+      clienterror(fd, filename, "403", "Forbidden",
+                  "Tinycouldn’treadthefile");
+      return;
+    }
+    serve_static(fd, filename, sbuf.st_size);
+  }
+  else
+  { /*Servedynamiccontent*/
+    if (!(S_ISREG(sbuf.st_mode)) || !(S_IXUSR & sbuf.st_mode))
+    {
+      clienterror(fd, filename, "403", "Forbidden",
+                  "Tinycouldn’truntheCGIprogram");
+      return;
+    }
+    serve_dynamic(fd, filename, cgiargs);
   }
 }
